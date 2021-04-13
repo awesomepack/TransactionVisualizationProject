73c5c2b84020760765c03a65c46028dc0de8ff6d
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-04-12 20:05:09 -0700

    Adding the JupyterDump.py and Excel Data Sheet

---
+++ JupyterDump.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+
+All of the code I have written so far for the TransactionVisualizationProject 
+will be stored here , to be broken up into manageable chunks
+
+"""
+
+
+
+    
+    
+
+    
+    
+    
+    
+    
+    
